1ab03e9dc2bd65fc3727924f43f8be1eca59c52c
FireCubeStudios <ayushmaan.b27@outlook.com>
2020-06-27 15:43:07 +0200

    Updates

---
+++ precache-manifest.273a69b91fcfa4b91ea62cbfd5b2dbd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e39ec09a7f78ca18f69b1cd34b23b18f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de29854533a1060b261c",
+    "url": "/static/css/main.7591def3.chunk.css"
+  },
+  {
+    "revision": "4612c4c0cb57417149eb",
+    "url": "/static/js/2.3252dd38.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.3252dd38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de29854533a1060b261c",
+    "url": "/static/js/main.036b1531.chunk.js"
+  },
+  {
+    "revision": "bb480f6fbd79a73e0cfe",
+    "url": "/static/js/runtime-main.776c2850.js"
+  }
+]);
